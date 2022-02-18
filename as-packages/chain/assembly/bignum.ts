5f73afb54d01ad250fa07274ec772c35d48b2d29
learnforpractice <learnforpractice@gmail.com>
2022-02-18 20:42:57 +0800

    implemented Packer for I128

--- as-packages/chain/assembly/bignum.ts
+++ as-packages/chain/assembly/bignum.ts
@@ -1,5 +1,25 @@
-import { u128, u256 } from "as-bignum"
+import { i128, u128, u256 } from "as-bignum"
 import { Encoder, Decoder, Packer } from "./serializer"
+
+export class I128 extends i128 implements Packer {
+    pack(): u8[] {
+        let enc = new Encoder(16);
+        enc.packNumber<u64>(this.lo);
+        enc.packNumber<i64>(this.hi);
+        return enc.getBytes();
+    }
+
+    unpack(data: u8[]): usize {
+        let dec = new Decoder(data);
+        this.lo = dec.unpackNumber<u64>();
+        this.hi = dec.unpackNumber<i64>();
+        return 16;
+    }
+
+    getSize(): usize {
+        return 16;
+    }
+}
 
 export class U128 extends u128 implements Packer {
     pack(): u8[] {
