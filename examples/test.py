25f92cd4d8826bfd729976cb72b53995eb6efeae
learnforpractice <learnforpractice@gmail.com>
2022-02-19 12:53:50 +0800

    Add inline action example

--- examples/test.py
+++ examples/test.py
@@ -13,6 +13,24 @@ logger = log.get_logger(__name__)
 
 chain = ChainTester()
 
+test_account1 = 'hello'
+a = {
+    "account": test_account1,
+    "permission": "active",
+    "parent": "owner",
+    "auth": {
+        "threshold": 1,
+        "keys": [
+            {
+                "key": 'EOS6AjF6hvF7GSuSd4sCgfPKq5uWaXvGM2aQtEUCwmEHygQaqxBSV',
+                "weight": 1
+            }
+        ],
+        "accounts": [{"permission":{"actor":test_account1,"permission": 'eosio.code'}, "weight":1}],
+        "waits": []
+    }
+}
+chain.push_action('eosio', 'updateauth', a, {test_account1:'active'})
 
 def test_hello():
     # info = chain.get_account('helloworld11')
@@ -23,3 +41,15 @@ def test_hello():
     r = chain.push_action('hello', 'sayhello', b'', {'hello': 'active'})
     logger.info('++++++elapsed: %s', r['elapsed'])
 
+def test_action():
+    with open('./inlineaction/target/target.wasm', 'rb') as f:
+        code = f.read()
+    with open('~lib/rt/target/generated.abi', 'rb') as f:
+        abi = f.read()
+    chain.deploy_contract('hello', code, abi, 0)
+
+    args = dict(
+        name = 'bob'
+    )
+    r = chain.push_action('hello', 'sayhello', args, {'hello': 'active'})
+    logger.info('++++++elapsed: %s', r['elapsed'])
