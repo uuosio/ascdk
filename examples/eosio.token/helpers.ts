da59665f59d1562ff4fa9a7c66a7a51082fc7468
jafri <syedjafri97@gmail.com>
2022-02-19 23:43:16 -0700

    Cleanup eosio.token example using destructuring

--- examples/eosio.token/helpers.ts
+++ examples/eosio.token/helpers.ts
@@ -1,23 +1,23 @@
-import * as chain from 'as-chain';
+import { Name, Symbol, Asset, check } from 'as-chain';
 import { createTokenContract } from './eosio.token';
 
-export function getSupply(tokenContractAccount: chain.Name, sym: chain.Symbol): chain.Asset {
+export function getSupply(tokenContractAccount: Name, sym: Symbol): Asset {
     const contract = createTokenContract(tokenContractAccount);
     const statstable = contract.getStatTable(sym);
 
     const existing = statstable.find(sym.code());
-    chain.check(existing.isOk(), "token with symbol does not exist");
+    check(existing.isOk(), "token with symbol does not exist");
     const st = statstable.get(existing);
 
     return st.supply;
 }
 
-export function getBalance(tokenContractAccount: chain.Name, owner: chain.Name, sym: chain.Symbol): chain.Asset {
+export function getBalance(tokenContractAccount: Name, owner: Name, sym: Symbol): Asset {
     const contract = createTokenContract(tokenContractAccount);
     const acnts = contract.getAccountsTable(owner);
 
     const existing = acnts.find(sym.code());
-    chain.check(existing.isOk(), "no balance object found");
+    check(existing.isOk(), "no balance object found");
     const ac = acnts.get(existing);
 
     return ac.balance;
