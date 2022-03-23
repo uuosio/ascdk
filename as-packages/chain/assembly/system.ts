68beb6429544204212538c1e9b8b08e770dd3fe7
jafri <syedjafri97@gmail.com>
2022-03-22 19:06:14 -0600

    Match assert behavior of eosio.cdt

--- as-packages/chain/assembly/system.ts
+++ as-packages/chain/assembly/system.ts
@@ -1,13 +1,11 @@
 import * as env from "./env";
 import { TimePoint } from "./time";
 
-export declare function eosio_assert_message(test: u32, msg_ptr: usize, msg_len: usize): void;
-
 export function assert(test: bool, msg: string): void {
     let s1 = String.UTF8.encode(msg);
     let dv = new DataView(s1);
     let _test = test ? 1 : 0;
-    env.eosio_assert_message(_test, dv.dataStart, msg.length);
+    env.eosio_assert(_test, dv.dataStart);
 }
 
 export function check(test: bool, msg: string): void {
