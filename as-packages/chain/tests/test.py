66cd89ada97265bc4b308e33727eda569b4c2a45
learnforpractice <learnforpractice@gmail.com>
2022-03-24 10:44:08 +0800

    Disable contracts console

--- as-packages/chain/tests/test.py
+++ as-packages/chain/tests/test.py
@@ -7,15 +7,15 @@ test_dir = os.path.dirname(__file__)
 sys.path.append(os.path.join(test_dir, '..'))
 
 from ipyeos import log
-from ipyeos.chaintester import ChainTester
-
+from ipyeos import chaintester
+chaintester.chain_config['contracts_console'] = False
 logger = log.get_logger(__name__)
 
 chain = None
 def chain_test(fn):
     def call(*args, **vargs):
         global chain
-        chain = ChainTester()
+        chain = chaintester.ChainTester()
 
         test_account1 = 'hello'
         a = {
