876b320622b99115775901587396c20aa40792c5
jafri <syedjafri97@gmail.com>
2022-03-25 01:08:01 -0600

    update idxUpdate

--- as-packages/chain/assembly/mi.ts
+++ as-packages/chain/assembly/mi.ts
@@ -171,7 +171,7 @@ export class MultiIndex<T extends MultiIndexValue> {
         let primaryIt = this.find(it.primary);
         let value = this.get(primaryIt);
         value.setSecondaryValue(it.dbIndex, idxValue);
-        this.update(primaryIt, value, payer);
+        this.db.update(primaryIt.i, payer.N, value.pack());
         this.idxdbs[it.dbIndex].updateEx(it, idxValue, payer.N);
     }
 
