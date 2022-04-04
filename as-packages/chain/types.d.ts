ad7357eaf7b78132a8ec1a6a88eb423ac7919080
jafri <syedjafri97@gmail.com>
2022-04-04 10:27:48 -0600

    Allow more parameters to table decorator

--- as-packages/chain/types.d.ts
+++ as-packages/chain/types.d.ts
@@ -6,7 +6,7 @@ declare function contract(_?: any): any
 declare function action(_?: any, __?: any): any
 declare function ignore(_?: string): any
 declare function packed(_?: string): any
-declare function table(_?: any, __?: string): any
+declare function table(_?: any, __?: string, ___?: string, ____?: string): any
 declare function variant(_?: any, __?: any): any
 declare function serializer(_?: any): any
 declare function primary(_?: any, __?: any): any
